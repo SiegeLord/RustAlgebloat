8c149b11344d45aba321005733b2c5e26f8a7fb1
Travis <travis@travis-ci.org>
2014-06-01 17:54:53 +0000

    Update docs from Travis build 64

---
+++ doc/implementors/algebloat/bin_funs/trait.VectorHypotOp.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['algebloat'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> + <a class='trait' href='algebloat/traits/trait.MatrixShape.html' title='algebloat::traits::MatrixShape'>MatrixShape</a>, RHS: <a class='trait' href='http://doc.rust-lang.org/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> + <a class='trait' href='algebloat/traits/trait.SameShape.html' title='algebloat::traits::SameShape'>SameShape</a>&gt; <a class='trait' href='algebloat/bin_funs/trait.VectorHypotOp.html' title='algebloat::bin_funs::VectorHypotOp'>VectorHypotOp</a>&lt;RHS&gt; for T",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/algebloat/reductions/trait.MatrixReduce.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['algebloat'] = ["impl&lt;T: <a class='trait' href='algebloat/traits/trait.MatrixGet.html' title='algebloat::traits::MatrixGet'>MatrixGet</a>&lt;<a href='http://doc.rust-lang.org/core/primitive.uint.html'>uint</a>&gt; + <a class='trait' href='http://doc.rust-lang.org/core/container/trait.Container.html' title='core::container::Container'>Container</a>&gt; <a class='trait' href='algebloat/reductions/trait.MatrixReduce.html' title='algebloat::reductions::MatrixReduce'>MatrixReduce</a> for T",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
