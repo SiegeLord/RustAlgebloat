f1067a8f168d915825456adef40a013ac6a14cf9
Travis <travis@travis-ci.org>
2014-06-01 23:04:33 +0000

    Update docs from Travis build 65

---
+++ doc/implementors/algebloat/traits/trait.MatrixReshape.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['algebloat'] = ["impl&lt;T: <a class='trait' href='algebloat/traits/trait.MatrixShape.html' title='algebloat::traits::MatrixShape'>MatrixShape</a> + <a class='trait' href='http://doc.rust-lang.org/core/container/trait.Container.html' title='core::container::Container'>Container</a>&gt; <a class='trait' href='algebloat/traits/trait.MatrixReshape.html' title='algebloat::traits::MatrixReshape'>MatrixReshape</a> for T",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
