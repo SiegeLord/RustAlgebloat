4e2d0ae943273afa49f1f5d6e0b14d1bebf7d0bb
SiegeLord <slabode@aim.com>
2014-06-01 13:52:52 -0400

    Forgot to add the file

---
+++ src/reductions.rs
@@ -0,0 +1,71 @@
+// Copyright (c) 2014 by SiegeLord
+//
+// All rights reserved. Distributed under LGPL 3.0. For full terms see the file LICENSE.
+
+use traits::MatrixGet;
+
+pub trait MatrixReduce
+{
+	fn min(&self) -> Option<(uint, f64)>;
+	fn max(&self) -> Option<(uint, f64)>;
+}
+
+impl<T: MatrixGet<uint> + Container>
+MatrixReduce for
+T
+{
+	fn min(&self) -> Option<(uint, f64)>
+	{
+		let l = self.len();
+		if l == 0
+		{
+			None
+		}
+		else
+		{
+			let mut ret = self.get(0);
+			let mut ret_i = 0;
+			for i in range(1, l)
+			{
+				unsafe
+				{
+					let c = self.unsafe_get(i);
+					if c < ret
+					{
+						ret = c;
+						ret_i = i;
+					}
+				}
+			}
+			Some((ret_i, ret))
+		}
+	}
+
+	fn max(&self) -> Option<(uint, f64)>
+	{
+		let l = self.len();
+		if l == 0
+		{
+			None
+		}
+		else
+		{
+			let mut ret = self.get(0);
+			let mut ret_i = 0;
+			for i in range(1, l)
+			{
+				unsafe
+				{
+					let c = self.unsafe_get(i);
+					if c > ret
+					{
+						ret = c;
+						ret_i = i;
+					}
+				}
+			}
+			Some((ret_i, ret))
+		}
+	}
+}
+
