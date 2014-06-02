ce9fd856e7fb236597c5d078386bb27c6197110a
Travis <travis@travis-ci.org>
2014-06-02 00:39:29 +0000

    Update docs from Travis build 66

--- doc/implementors/algebloat/traits/trait.ToMatrix.js
+++ doc/implementors/algebloat/traits/trait.ToMatrix.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['algebloat'] = ["impl&lt;T: <a class='trait' href='algebloat/traits/trait.MatrixShape.html' title='algebloat::traits::MatrixShape'>MatrixShape</a> + <a class='trait' href='algebloat/traits/trait.MatrixGet.html' title='algebloat::traits::MatrixGet'>MatrixGet</a>&lt;<a href='http://doc.rust-lang.org/std/primitive.uint.html'>uint</a>&gt; + <a class='trait' href='http://doc.rust-lang.org/core/container/trait.Container.html' title='core::container::Container'>Container</a>&gt; <a class='trait' href='algebloat/traits/trait.ToMatrix.html' title='algebloat::traits::ToMatrix'>ToMatrix</a> for T",];
+implementors['algebloat'] = ["impl&lt;T: <a class='trait' href='algebloat/traits/trait.MatrixShape.html' title='algebloat::traits::MatrixShape'>MatrixShape</a> + <a class='trait' href='algebloat/traits/trait.MatrixGet.html' title='algebloat::traits::MatrixGet'>MatrixGet</a>&lt;<a href='http://doc.rust-lang.org/core/primitive.uint.html'>uint</a>&gt; + <a class='trait' href='http://doc.rust-lang.org/core/container/trait.Container.html' title='core::container::Container'>Container</a>&gt; <a class='trait' href='algebloat/traits/trait.ToMatrix.html' title='algebloat::traits::ToMatrix'>ToMatrix</a> for T",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
