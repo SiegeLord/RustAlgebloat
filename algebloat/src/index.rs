309883a0744881f7b2babad48d9231b8a6da68db
SiegeLord <slabode@aim.com>
2014-07-13 07:38:31 -0400

    I hate macro hygiene

--- algebloat/src/index.rs
+++ algebloat/src/index.rs
@@ -18,29 +18,31 @@ pub trait MatrixIndexSet<T>
 
 macro_rules! index_impl
 {
-	($idx_type: ty, $rc_expr: expr) =>
+	($self_: ident, $mat: ident, $idx_type: ty, $rc_expr: expr) =>
 	{
 		impl<T: MatrixRawGet + MatrixShape>
 		     MatrixIndexGet<T> for
 		$idx_type
 		{
-			fn unsafe_get_idx(&self, mat: &T) -> f64
+			fn unsafe_get_idx(&self, $mat: &T) -> f64
 			{
+				let $self_ = self;
 				let (r, c) = $rc_expr;
 				unsafe
 				{
-					mat.raw_get(r, c)
+					$mat.raw_get(r, c)
 				}
 			}
 
-			fn get_idx(&self, mat: &T) -> f64
+			fn get_idx(&self, $mat: &T) -> f64
 			{
+				let $self_ = self;
 				let (r, c) = $rc_expr;
-				assert!(r < mat.nrow());
-				assert!(c < mat.ncol());
+				assert!(r < $mat.nrow());
+				assert!(c < $mat.ncol());
 				unsafe
 				{
-					mat.raw_get(r, c)
+					$mat.raw_get(r, c)
 				}
 			}
 		}
@@ -49,23 +51,25 @@ macro_rules! index_impl
 		     MatrixIndexSet<T> for
 		$idx_type
 		{
-			fn unsafe_set_idx(&self, mat: &T, v: f64)
+			fn unsafe_set_idx(&self, $mat: &T, v: f64)
 			{
+				let $self_ = self;
 				let (r, c) = $rc_expr;
 				unsafe
 				{
-					mat.raw_set(r, c, v)
+					$mat.raw_set(r, c, v)
 				}
 			}
 
-			fn set_idx(&self, mat: &T, v: f64)
+			fn set_idx(&self, $mat: &T, v: f64)
 			{
+				let $self_ = self;
 				let (r, c) = $rc_expr;
-				assert!(r < mat.nrow());
-				assert!(c < mat.ncol());
+				assert!(r < $mat.nrow());
+				assert!(c < $mat.ncol());
 				unsafe
 				{
-					mat.raw_set(r, c, v)
+					$mat.raw_set(r, c, v)
 				}
 			}
 		}
@@ -77,7 +81,7 @@ fn to_rc(idx: uint, ncol: uint) -> (uint, uint)
 	(idx / ncol, idx % ncol)
 }
 
-index_impl!((uint, uint), {*self})
-index_impl!((int, int), {let (r, c) = *self; (r as uint, c as uint)})
-index_impl!(uint, {to_rc(*self, mat.ncol())})
-index_impl!(int,  {to_rc(*self as uint, mat.ncol())})
+index_impl!(self_, mat, (uint, uint), {*self_})
+index_impl!(self_, mat, (int, int), {let (r, c) = *self_; (r as uint, c as uint)})
+index_impl!(self_, mat, uint, {to_rc(*self_, mat.ncol())})
+index_impl!(self_, mat, int,  {to_rc(*self_ as uint, mat.ncol())})
