731d9d405927deb3f6bf34dc2ee2ef261fa39b16
Travis <travis@travis-ci.org>
2014-11-03 13:33:23 +0000

    Update docs from Travis build 93

---
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['algebloat'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: <a class='trait' href='algebloat/traits/trait.MatrixGet.html' title='algebloat::traits::MatrixGet'>MatrixGet</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.uint.html'>uint</a>&gt; + <a class='trait' href='algebloat/traits/trait.MatrixShape.html' title='algebloat::traits::MatrixShape'>MatrixShape</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.f64.html'>f64</a>&gt; for <a class='struct' href='algebloat/elements/struct.MatrixElements.html' title='algebloat::elements::MatrixElements'>MatrixElements</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
