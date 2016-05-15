b4c0a52b3f6ca4e7bf58f2675c93d9d30c22a899
Travis <travis@travis-ci.org>
2016-05-15 02:05:59 +0000

    Update docs from Travis build 380

--- doc/implementors/algebloat/un_funs/trait.Inv.js
+++ doc/implementors/algebloat/un_funs/trait.Inv.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['algebloat'] = [];
+implementors["algebloat"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
