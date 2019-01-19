9b915ee8656a873948df9feba0c75ac82edd9c1b
Travis <travis@travis-ci.org>
2019-01-19 15:04:55 +0000

    Update docs from Travis build 1363

--- doc/source-files.js
+++ doc/source-files.js
@@ -2,3 +2,4 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["algebloat"] = {"name":"","dirs":[],"files":["bin_funs.rs","bin_ops.rs","column_accessor.rs","elements.rs","hstack.rs","index.rs","lib.rs","matrix.rs","matrix_mul.rs","reductions.rs","reshape.rs","row_accessor.rs","scalar.rs","slice.rs","traits.rs","transpose.rs","un_funs.rs","un_ops.rs","view.rs","vstack.rs"]};
 sourcesIndex["algebloat_macros"] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex["delete_me"] = {"name":"","dirs":[],"files":["dummy.rs"]};
+createSourceSidebar();
