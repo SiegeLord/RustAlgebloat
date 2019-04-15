1426c7bc097ab325d4917936e42b155e06eba630
Travis <travis@travis-ci.org>
2019-04-15 18:03:51 +0000

    Update docs from Travis build 1449

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["algebloat"] = {"name":"","dirs":[],"files":["bin_funs.rs","bin_ops.rs","column_accessor.rs","elements.rs","hstack.rs","index.rs","lib.rs","matrix.rs","matrix_mul.rs","reductions.rs","reshape.rs","row_accessor.rs","scalar.rs","slice.rs","traits.rs","transpose.rs","un_funs.rs","un_ops.rs","view.rs","vstack.rs"]};
-sourcesIndex["algebloat_macros"] = {"name":"","dirs":[],"files":["lib.rs"]};
-sourcesIndex["delete_me"] = {"name":"","dirs":[],"files":["dummy.rs"]};
+sourcesIndex["algebloat"] = {"name":"","files":["bin_funs.rs","bin_ops.rs","column_accessor.rs","elements.rs","hstack.rs","index.rs","lib.rs","matrix.rs","matrix_mul.rs","reductions.rs","reshape.rs","row_accessor.rs","scalar.rs","slice.rs","traits.rs","transpose.rs","un_funs.rs","un_ops.rs","view.rs","vstack.rs"]};
+sourcesIndex["algebloat_macros"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["delete_me"] = {"name":"","files":["dummy.rs"]};
 createSourceSidebar();
